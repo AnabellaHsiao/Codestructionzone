ad08b41dfffe3de3612d6c76b657a3329b51584d
AnabellaHsiao <92648601+AnabellaHsiao@users.noreply.github.com>
2023-03-07 14:08:13 -0500

    feat:project added app.js css

--- static/question1.js
+++ static/question1.js
@@ -1,6 +1,14 @@
-var editor = monaco.editor.create(document.getElementById("container"), {
+let editor = monaco.editor.create(document.getElementById("container"), {
   value: ["function x() {", '\tconsole.log("Nevinn is not okay");', "}"].join(
     "\n"
   ),
   language: "javascript",
 });
+
+let editor2 = monaco.editor.create(
+  document.getElementById("output-container"),
+  {
+    value: ['\tconsole.log("Nevinn is not okay");'].join("\n"),
+    language: "javascript",
+  }
+);
