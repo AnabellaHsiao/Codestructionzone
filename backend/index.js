11be5b7f17121eb08806cc14a867107cb9e672f1
Nevin Wong <nevin.wong@mail.utoronto.ca>
2023-03-07 14:40:49 -0500

    feat:backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const Axios = require("axios");
+const axios = require("axios");
 const app = express();
 const PORT = 8000;
 
@@ -10,34 +10,57 @@ app.use(express.json());
 app.post("/compile", (req, res) => {
   //getting the required data from the request
   let code = req.body.code;
-  let language = req.body.language;
+  //   let language = req.body.language;
   let input = req.body.input;
 
-  if (language === "python") {
-    language = "py";
-  }
+  //   if (language === "python") {
+  //     language = "py";
+  //   }
 
-  let data = {
-    code: code,
-    language: language,
-    input: input,
-  };
-  let config = {
-    method: "post",
-    url: "https://ce.judge0.com",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    data: data,
-  };
+  //   let data = {
+  //     code: code,
+  //     language: language,
+  //     input: input,
+  //   };
+  //   let config = {
+  //     method: "post",
+  //     url: "https://ce.judge0.com",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     data: data,
+  //   };
   //calling the code compilation API
-  Axios(config)
-    .then((response) => {
-      res.send(response.data);
+
+  const options = {
+    method: "POST",
+    url: "https://judge0-ce.p.rapidapi.com/submissions/?base64_encoded=false&wait=true",
+    headers: {
+      "X-RapidAPI-Key": "d54f68f664msh4dcc91fed2cd856p1f9862jsn60ced304edec",
+      "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+    },
+    body: {
+      source_code: "console.log('yes');",
+      language_id: 63,
+    },
+  };
+
+  //   Axios(options)
+  //     .then((response) => {
+  //       res.send(response.data);
+  //       console.log(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  console.log("here");
+  axios
+    .request(options)
+    .then(function (response) {
       console.log(response.data);
     })
-    .catch((error) => {
-      console.log(error);
+    .catch(function (error) {
+      console.error(error);
     });
 });
 
